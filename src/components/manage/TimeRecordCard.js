385e9ce866d16868f1662dfdd515245cc9fdb7be
heesoo <11junghs@gmail.com>
2022-02-18 13:22:13 +0900

    Revert "Revert "Merge branch 'main' of https://github.com/CSE-Final-Project/Front""
    This reverts commit a21f68a3ddc50b2adf34061dfb7a26e9257d71ad.

--- src/components/manage/TimeRecordCard.js
+++ src/components/manage/TimeRecordCard.js
@@ -3,6 +3,10 @@ import {Card} from 'react-bootstrap';
 
 const TimeRecordCard = (props) => {
     const timeInfo = props.timeInfo;
+    const h = parseInt(timeInfo.studytime/3600);
+    const m = parseInt((timeInfo.studytime%3600)/60);
+    const s = timeInfo.studytime%60;
+    console.log('TimeRecordCard 전달 데이터:', timeInfo);
     return (
         <div>
             <br/>
@@ -12,7 +16,7 @@ const TimeRecordCard = (props) => {
                     <blockquote className="blockquote mb-0">
                     <p>
                         {' '}
-                        {timeInfo.total_time}시간{' '}
+                        {h}시간{' '}{m}분{' '}{s}초{' '}
                     </p>
                     
                     </blockquote>
