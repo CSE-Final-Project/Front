394b7066fa2adc42fb5f772c7d4911d6a4b8e916
MinjaeJo1999 <rycbar1999@naver.com>
2022-05-05 12:45:34 +0900

    [5월 5일] 소켓 에러 수정
    - 전송 데이터값 수정

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -5,11 +5,57 @@ const StopWatch = (props) => {
     const [stop, setStop] = useState(false);
     const [click, setClick] = useState(false);
     const socket = props.socket;
+
+    //지연 수정
+   // const [delay, setDelay] = React.useState(1000); 
+    //const [isRunning, toggleIsRunning] = useBoolean(true); 
     
     let stop_val = true;
 
+<<<<<<< Updated upstream
     const sendWatchValue= () =>{
         if(props.watch === 'false'){
+=======
+    //함수로 바꿔서 const StopWatch 밖으로 내보내기, 
+    //stopwatch에서 useInterval 함수 사용하기
+    //useEffect 부분 조건 따라 isRunning 값 바꾸기 
+    //(mode on, off 둘 다 isRunning 으로 사용..그럼 이 함수가 Room에 가야할듯.. mode 바뀔 때도 초기화, 디텍션할 때도)
+    /*const useInterval = (callback, delay)=> {
+        const savedCallback = useRef(); // 최근에 들어온 callback을 저장할 ref를 하나 만든다.
+      
+        useEffect(() => {
+          savedCallback.current = callback; // callback이 바뀔 때마다 ref를 업데이트 해준다.
+        }, [callback]);
+      
+        useEffect(() => {
+          function tick() {
+            savedCallback.current(); // tick이 실행되면 callback 함수를 실행시킨다.
+          }
+          if (delay !== null) { // 만약 delay가 null이 아니라면 
+            let id = setInterval(tick, delay); // delay에 맞추어 interval을 새로 실행시킨다.
+            return () => clearInterval(id); // unmount될 때 clearInterval을 해준다.
+          }
+        }, [delay]); // delay가 바뀔 때마다 새로 실행된다.
+      }
+*/
+
+    const sendWatchValue= () =>{ //스탑워치 클릭 시 로직
+        props.getClickValue(!props.click);
+        if(!props.mode){ //자동 측정 mode off 일 때만 watch 수동 조작 이벤트 실행
+            if(props.watch === 'false'){
+                props.getWatchValue('true');
+                props.timeStart();
+                props.getVideoColorValue('true') //흑백 - 블랙 타이밍 확인용
+                socket.emit('false-event', { peer_tf: props.myID, dst_room: props.roomID, tf_state: 'true'})
+            }else{
+                props.getWatchValue('false');
+                props.timeEnd();
+                props.getVideoColorValue('false') //흑백 - 블랙 타이밍 확인용
+                socket.emit('false-event', { peer_tf: props.myID, dst_room: props.roomID, tf_state: 'false'})
+            }
+        }else{ //mode on
+            props.getClickValue(true);
+>>>>>>> Stashed changes
             props.getWatchValue('true');
             socket.emit('false-event', { peer_tf: props.myID, dst_room: props.roomID, tf_state: 'true'})
         }else{
@@ -30,6 +76,7 @@ const StopWatch = (props) => {
         let interval = null
 
         setStop(stop_val);
+<<<<<<< Updated upstream
         // console.log('aaa: ',stop);
         
         if(click & stop){
@@ -38,16 +85,39 @@ const StopWatch = (props) => {
             },10)
         } else {
              clearInterval(interval);
+=======
+        console.log('aaa: ',stop);
+        //console.log("시간 측정 기준 변수 상태: click->", props.click, " stop->", stop," props.watch->", props.watch)
+        // mode on: stop만 true 면 됨
+        if(props.mode){  
+            if(stop){//mode on, running -> true
+                interval = setInterval(()=>{
+                    setTime(prevTime => prevTime+10)
+                },10)
+            } else { //mode off, not running -> false
+                clearInterval(interval);
+            }
+        }else{ //mode off: watch 만 true 면 됨 (코드 나중에 정리)
+            if(props.click &  props.watch === 'true' ){  //mode off, running -> true
+                interval = setInterval(()=>{
+                    setTime(prevTime => prevTime+10)
+                },10)
+            } else { //mode off , running -> false
+                clearInterval(interval);
+            }
+
+>>>>>>> Stashed changes
         }
 
         return () => clearInterval(interval);
     },)
 
+    
     const buttonStyle = {
         position: 'absolute', 
         right: '0px', 
         bottom: '0px',
-        fontSize: '0.7rem',
+        fontSize: '1.5rem', //원래 0.7 (발표용)
         color: 'white',
         backgroundColor: 'rgba(0, 0, 0, 0.7)',
         fontWeight: 'bold',
@@ -68,9 +138,16 @@ const StopWatch = (props) => {
             else if(document.msExitFullscreen){ document.msExitFullscreen(); }
         }
     }
+<<<<<<< Updated upstream
 
     return (
             <button style = {buttonStyle} onClick={()=>{ fullChange(); setClick(!click); sendWatchValue();}}>
+=======
+   // fullChange();
+    //setClick(!click);
+    return (
+            <button style = {buttonStyle} onClick={()=>{  sendWatchValue();  fullChange();}}>
+>>>>>>> Stashed changes
                 <span>{("0" + Math.floor((time/3600000)%24)).slice(-2)}:</span>
                 <span>{("0" + Math.floor((time/60000)%60)).slice(-2)}:</span>
                 <span>{("0" + Math.floor((time/1000)%60)).slice(-2)}:</span>
