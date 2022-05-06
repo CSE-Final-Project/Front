84a2de17a8ed78fbdfba6b9dd9e2a7ac138d0717
heesoo <11junghs@gmail.com>
2022-05-06 16:41:11 +0900

    [5월 6일] deploy_test_1

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
-const client = axios.create();
+// const client = axios.create({
+//     withCredentials: true
+// });
+
+// const client = axios.create();
+
+const client = axios.create({
+    withCredentials: true
+})
 
 export default client;
