f137f5ab8731f6d0594063d829bf2ce43fee64bf
heesoo <11junghs@gmail.com>
2022-05-12 19:35:11 +0900

    [5월 12일] 00:00:00형식

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -130,7 +130,7 @@ const StopWatch = (props) => {
             <button style = {buttonStyle} onClick={()=>{  sendWatchValue();  fullChange();}}>
                 <span>{("0" + Math.floor((time/3600000)%24)).slice(-2)}:</span>
                 <span>{("0" + Math.floor((time/60000)%60)).slice(-2)}:</span>
-                {/* <span>{("0" + Math.floor((time/1000)%60)).slice(-2)}:</span> */}
+                <span>{("0" + Math.floor((time/1000)%60)).slice(-2)}</span>
                 {/* <span>{("0" + (time/10)%1000).slice(-2)}</span> */}
             </button>
 
